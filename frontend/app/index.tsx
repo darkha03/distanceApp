cc91ca2dcb7c08f66259d0a34401fbb20cd99323
darkhan03 <steve.khanhnguyen@gmail.com>
2025-10-05 23:58:09 +0200

    add initial screen

---
+++ frontend/app/index.tsx
@@ -0,0 +1,18 @@
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+
+export default function IndexScreen() {
+  const { token } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (token) {
+      router.replace("/dashboard/(tabs)");
+    } else {
+      router.replace("/auth/login");
+    }
+  }, [token]);
+
+  return null; // Or a loading spinner
+}
